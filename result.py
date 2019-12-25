92eb1446332d602b94dd422fc940eccbd570bc85
wojciech.skublewski@wp.pl <wojciech.skublewski@wp.pl>
2019-12-25 08:33:25 +0100

    cd

--- result.py
+++ result.py
@@ -1,8 +1,10 @@
 import db_con
+import datetime
+
 from psycopg2.extras import RealDictCursor
 
-def addResult(brick_id, result):
-    sql = f"insert into brick_results(brick_id, result) values ('{brick_id}', '{result}') returning result_id"
+def addResult(brick_id, result, user_id=1):
+    sql = f"insert into brick_results(brick_id, user_id, result_date, result_time, result) values ('{brick_id}', '{user_id}', '{datetime.datetime.now().date()}', '{datetime.datetime.now().time().strftime('%H:%M:%S')}','{result}') returning result_id"
     cnx = db_con.create_connection("warsztat1")
     cursor = cnx.cursor()
 
@@ -11,3 +13,9 @@ def addResult(brick_id, result):
     cursor.close()
     cnx.close()
 
+
+
+
+addResult(1,5,1)
+
+print(datetime.datetime.now().date())
